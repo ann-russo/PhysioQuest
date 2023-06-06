beb3b94f9aad22a72d93d0b3e341cbbc079b1b7d
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2023-06-06 21:35:52 +0200

    Corrected issue with redirect to Home screen when user exists

--- app/src/main/java/com/example/physioquest/screens/start/StartViewModel.kt
+++ app/src/main/java/com/example/physioquest/screens/start/StartViewModel.kt
@@ -2,17 +2,23 @@ package com.example.physioquest.screens.start
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.physioquest.HOME_SCREEN
 import com.example.physioquest.START_SCREEN
 import com.example.physioquest.WELCOME_SCREEN
+import com.example.physioquest.service.AccountService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class StartViewModel @Inject constructor() : ViewModel() {
+class StartViewModel @Inject constructor(private val accountService: AccountService) : ViewModel() {
     val showError = mutableStateOf(false)
 
     fun onAppStart(openAndPopUp: (String, String) -> Unit) {
         showError.value = false
-        openAndPopUp(WELCOME_SCREEN, START_SCREEN)
+        if (accountService.hasUser) {
+            openAndPopUp(HOME_SCREEN, START_SCREEN)
+        } else {
+            openAndPopUp(WELCOME_SCREEN, START_SCREEN)
+        }
     }
 }
