ee40c5521913554f10f5db216622c838010c189e
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2023-06-07 12:06:57 +0200

    Added confirm dialog to Lernmodus when closing the quiz

---
+++ app/src/main/java/com/example/physioquest/common/composable/AnimatedDialogComposable.kt
@@ -0,0 +1,61 @@
+package com.example.physioquest.common.composable
+
+import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.physioquest.R
+
+@Composable
+fun AnimatedDialog(
+    visible: Boolean,
+    onClose: () -> Unit,
+    onConfirm: () -> Unit,
+    @StringRes title: Int,
+    @StringRes content: Int,
+    @StringRes actionButton: Int,
+) {
+    AnimatedVisibility(
+        visible = visible,
+        enter = scaleIn() + expandVertically(expandFrom = Alignment.CenterVertically),
+        exit = scaleOut() + shrinkVertically(shrinkTowards = Alignment.CenterVertically)
+    ) {
+        AlertDialog(
+            containerColor = Color.White,
+            onDismissRequest = { onClose() },
+            title = {
+                Text(stringResource(title))
+            },
+            text = {
+                Text(stringResource(content))
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = { onConfirm() }
+                ) {
+                    Text(
+                        text = stringResource(actionButton),
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = { onClose() }
+                ) {
+                    Text(stringResource(R.string.cancel))
+                }
+            }
+        )
+    }
+}
